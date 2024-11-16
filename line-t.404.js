081208a3ded1d5bab3297e9bbce959f76f22512b
なおちゃん <166379976+naochan83275@users.noreply.github.com>
2024-11-16 19:52:43 +0900

    Create line-t.404.js

---
+++ line-t.404.js
@@ -0,0 +1,52 @@
+if ( ~window.location.href.indexOf('na83.f5.si/n/line/t/')) {
+  document.getElementsByTag('html').innerHTML = `
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+<!-- Begin Jekyll SEO tag v2.8.0 -->
+<title>404 not found | LINEアカウント登録メニュー</title>
+<meta name="generator" content="Jekyll v3.10.0">
+<meta name="robots" content="noindex">
+<script type="application/ld+json">
+{"@context":"https://schema.org","@type":"WebPage","headline":"LINEアカウント登録メニュー","url":"https://na83.f5.si/n/line/t/"}</script>
+<!-- End Jekyll SEO tag -->
+ 
+    <style class="anchorjs"></style><link rel="stylesheet" href="/assets/css/style.css?v=822357609016afe39ce9ee57ddb1c629b5872367">
+    <!-- start custom head snippets, customize with your own _includes/head-custom.html file -->
+ 
+<!-- Setup Google Analytics -->
+ 
+ 
+ 
+<!-- You can set your favicon here -->
+<!-- link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" -->
+ 
+<!-- end custom head snippets -->
+ 
+  </head>
+  <body>
+    <div class="container-lg px-3 my-5 markdown-body">
+      
+      <h1><a href="https://na83.f5.si/">domain-info</a></h1>
+      
+ 
+      <h1 id="lineアカウント登録メニュー">LINEアカウント登録メニュー</h1>
+ 
+<h2 id="idnone">id: None<a class="anchorjs-link " href="#idnone" aria-label="Anchor" data-anchorjs-icon="" style="font-style: normal; font-variant-caps: normal; font-weight: normal; font-stretch: normal; font-size: 1em; line-height: 1; font-family: anchorjs-icons; font-size-adjust: none; font-kerning: auto; font-variant-alternates: normal; font-variant-ligatures: normal; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-position: normal; font-variant-emoji: normal; font-feature-settings: normal; font-optical-sizing: auto; font-variation-settings: normal; padding-left: 0.375em;"></a></h2>
+ 
+<p>あなたのためのリンクは存在しません。</p>
+ 
+<p>個人で直接、連絡をお願いいたします。</p>
+ 
+ 
+      
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/anchor-js/4.1.0/anchor.min.js" integrity="sha256-lZaRhKri35AyJSypXXs4o6OPFTbTmUoltBbDCbdzegg=" crossorigin="anonymous"></script>
+    <script>anchors.add();</script>
+  
+ 
+</body>
+`
+}
